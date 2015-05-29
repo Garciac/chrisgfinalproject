4119567da475d5516966dd49235f04be0322ae44
Chris Garcia <chrisg1245@hotmail.com>
2015-05-28 20:09:01 -0700

    no message

--- homepage.php
+++ homepage.php
@@ -49,49 +49,98 @@ and open the template in the editor.
         <!-- time and date -->
         <iframe src="http://free.timeanddate.com/clock/i4oy8u06/n137/tc2aabd2/tt0/tw0/tm1/th2/tb4" frameborder="0" width="89" height="34"></iframe>
 
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
             <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
             </ol>
-
+            <!-- 1ST instructions -->
+            <h2>How to register.</h2>
             <!-- Wrapper for slides -->
-            <div class="carousel-inner">
+            <div class="carousel-inner" role="listbox">
                 <div class="item active">
-                    <img class="img-responsive img-responsive" src="images/register.png" alt="Register">
+                    <img src="images/register.png" alt="Register">
                     <div class="carousel-caption">
-                        <h3>How to register</h3>
-                        <p>Click on Register.</p>
+                        <h3>Step One</h3>
+                        <p>To register, click on the Register Button on the top right corner.</p>
                     </div>
                 </div>
-                <!-- 2nd photo for carousel -->
+
                 <div class="item">
-                    <img class="img-responsive img-responsive" src="images/register.png" alt="CSS">
+                    <img src="images/register01.png" alt="Register">
                     <div class="carousel-caption">
-                        <h3>CSS</h3>
-                        <p>Cascading Style Sheets (CSS) is a style sheet language used for describing the look and formatting of a document written in a markup language. </p>
+                        <h3>Step Two</h3>
+                        <p>Next, you need to provide a valid E-Mail. Then, create a username and password. When you finished providing an E-Mail, Username, and Password, click on the Submit Button. </p>
                     </div>
                 </div>
-                <!-- 3rd photo for carousel -->
+
                 <div class="item">
-                    <img src="images/instructionstoregister3.jpg" alt="Bootstrap">
+                    <img src="images/register02.png" alt="Register">
                     <div class="carousel-caption">
-                        <h3>Bootstrap</h3>
-                        <p>Bootstrap is a free collection of tools for creating websites and web applications. It contains HTML and CSS-based design templates for typography, forms, buttons, navigation and other interface components, as well as optional JavaScript extensions. </p>
+                        <h3>Step Three</h3>
+                        <p>Finally, login with your Username and Password and click the Submit Button. You are now officially logged in.</p>
                     </div>
                 </div>
             </div>
 
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
             </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
             </a>
         </div>
+        <h2>How To Create a Post.</h2>
+        <!-- 2ND instructions -->
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <h2>How to register.</h2>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="images/register.png" alt="Register">
+                    <div class="carousel-caption">
+                        <h3>Step One</h3>
+                        <p>To register, click on the Register Button on the top right corner.</p>
+                    </div>
+                </div>
 
+                <div class="item">
+                    <img src="images/register01.png" alt="Register">
+                    <div class="carousel-caption">
+                        <h3>Step Two</h3>
+                        <p>Next, you need to provide a valid E-Mail. Then, create a username and password. When you finished providing an E-Mail, Username, and Password, click on the Submit Button. </p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="images/register02.png" alt="Register">
+                    <div class="carousel-caption">
+                        <h3>Step Three</h3>
+                        <p>Finally, login with your Username and Password and click the Submit Button. You are now officially logged in.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </body>
 </html>
